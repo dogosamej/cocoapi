590217225eb267a5969a864d937358e0f90d3067
dogosamej <wolf.hack.4@gmail.com>
2025-05-22 16:30:44 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
-const { isUserLive } = require('./liveChecker');
+const { isUserLive } = require('./livechecker');
 
 const app = express();
 const PORT = process.env.PORT || 10000;
