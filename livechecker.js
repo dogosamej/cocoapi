52178d9b3eb2cbddddb0311e2db7a355830c27ef
dogosamej <wolf.hack.4@gmail.com>
2025-05-23 10:25:26 -0500

    fix: actualizar livechecker con path correcto y más timeout

--- livechecker.js
+++ livechecker.js
@@ -4,17 +4,17 @@ async function isUserLive(username) {
   try {
     const browser = await puppeteer.launch({
       headless: 'new',
-      executablePath: '/usr/bin/chromium-browser', // usa el Chrome del contenedor
+      executablePath: '/usr/bin/chromium', // usa el Chrome del contenedor
       args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
 
     const page = await browser.newPage();
     await page.goto(`https://www.tiktok.com/@${username}`, {
       waitUntil: 'domcontentloaded',
-      timeout: 30000
+      timeout: 60000
     });
 
-    await page.waitForTimeout(2000);
+    await page.waitForTimeout(5000);
     const isLive = await page.$('.css-101726n-SpanLiveBadge.e1vl87hj3');
 
     await browser.close();
